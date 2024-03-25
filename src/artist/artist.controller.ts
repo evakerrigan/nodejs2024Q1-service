35bb9047cb0c23ee2e71012975e0deeb2ebbf55f
Eva Kerrigan <evakerrigan@gmail.com>
2024-03-26 04:02:29 +0600

    feat: module artists add to prisma

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -19,47 +19,89 @@ export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Get()
-  findAll(): Artist[] {
-    return this.artistService.findAll();
+  async findAll(): Promise<Artist[]> {
+    try {
+      const artists = await this.artistService.findAll();
+      return artists;
+    } catch (error) {
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    // return this.artistService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Artist {
+  async findOne(@Param('id') id: string): Promise<Artist> {
     if (!uuidValidate(id)) {
       throw new HttpException('Invalid UUID', HttpStatus.BAD_REQUEST);
     }
-    const artist = this.artistService.findOne(id);
-    if (!artist) {
-      throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
+    try {
+      const artist = await this.artistService.findOne(id);
+      if (!artist) {
+        throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
+      }
+      return artist;
+    } catch (error) {
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    return artist;
   }
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto): Artist {
-    return this.artistService.create(createArtistDto);
+  async create(@Body() createArtistDto: CreateArtistDto): Promise<Artist> {
+    try {
+      const artist = await this.artistService.create(createArtistDto);
+      return artist;
+    } catch (error) {
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    // return this.artistService.create(createArtistDto);
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateArtistDto: CreateArtistDto,
-  ): Artist {
+  ): Promise<Artist> {
     if (!uuidValidate(id)) {
       throw new HttpException('Invalid UUID', HttpStatus.BAD_REQUEST);
     }
-    return this.artistService.update(id, updateArtistDto);
+    try {
+      const artist = await this.artistService.update(id, updateArtistDto);
+      return artist;
+    } catch (error) {
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    // return this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string): void {
+  async remove(@Param('id') id: string): Promise<void> {
     if (!uuidValidate(id)) {
       throw new HttpException('Invalid UUID', HttpStatus.BAD_REQUEST);
     }
-    const isRemoved = this.artistService.remove(id);
-    if (!isRemoved) {
-      throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
+    try {
+      await this.artistService.remove(id);
+    } catch (error) {
+      throw new HttpException(
+        'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+    // const isRemoved = this.artistService.remove(id);
+    // if (!isRemoved) {
+    //   throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
+    // }
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
+import { PrismaClient, Artist as PrismaArtist } from '@prisma/client';
 import { CreateArtistDto } from './create-artist.dto';
 import { albums, artists, tracks } from 'src/database/db';
 
@@ -11,30 +12,46 @@ export interface Artist {
 
 @Injectable()
 export class ArtistService {
-  findAll(): Artist[] {
-    return artists;
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
   }
 
-  findOne(id: string): Artist {
-    return artists.find((artist) => artist.id === id);
+  async findAll(): Promise<PrismaArtist[]> {
+    return this.prisma.artist.findMany();
   }
 
-  create(createArtistDto: CreateArtistDto): Artist {
+  async findOne(id: string): Promise<PrismaArtist> {
+    const artist = await this.prisma.artist.findUnique({
+      where: { id },
+    });
+    if (!artist) {
+      throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
+    }
+    return artist;
+  }
+
+  async create(createArtistDto: CreateArtistDto): Promise<PrismaArtist> {
     if (!createArtistDto.name || !createArtistDto.grammy) {
       throw new HttpException(
         'Name, year are required',
         HttpStatus.BAD_REQUEST,
       );
     }
-    const newArtist: Artist = {
-      id: uuidv4(),
-      ...createArtistDto,
-    };
-    artists.push(newArtist);
+    const newArtist = await this.prisma.artist.create({
+      data: {
+        ...createArtistDto,
+        id: uuidv4(),
+      },
+    });
     return newArtist;
   }
 
-  update(id: string, updateArtistDto: CreateArtistDto): Artist {
+  async update(
+    id: string,
+    updateArtistDto: CreateArtistDto,
+  ): Promise<PrismaArtist> {
     const index = artists.findIndex((artist) => artist.id === id);
     if (index === -1) {
       throw new HttpException('Artist not found', HttpStatus.NOT_FOUND);
@@ -48,15 +65,19 @@ export class ArtistService {
         HttpStatus.BAD_REQUEST,
       );
     }
-    const updatedArtist: Artist = {
-      ...artists[index],
-      ...updateArtistDto,
-    };
-    artists[index] = updatedArtist;
+    const updatedArtist = await this.prisma.artist.update({
+      where: { id },
+      data: updateArtistDto,
+    });
+    // const updatedArtist: Artist = {
+    //   ...artists[index],
+    //   ...updateArtistDto,
+    // };
+    // artists[index] = updatedArtist;
     return updatedArtist;
   }
 
-  remove(id: string): boolean {
+  async remove(id: string): Promise<boolean> {
     const index = artists.findIndex((artist) => artist.id === id);
     if (index === -1) {
       return false;
@@ -71,7 +92,10 @@ export class ArtistService {
         album.artistId = null;
       }
     });
-    artists.splice(index, 1);
+    await this.prisma.artist.delete({
+      where: { id },
+    });
+    // artists.splice(index, 1);
     return true;
   }
 }
